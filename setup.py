62c507f35810717d791b6ec758e1af53b9f286c8
vaibhavjain2609 <vaibhavjain@protonmail.com>
2024-03-29 15:05:19 -0700

    chore: added requirements.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+Insta485 python package configuration.
+
+Andrew DeOrio <awdeorio@umich.edu>
+"""
+
+from setuptools import setup
+
+setup(
+    name='insta485',
+    version='0.1.0',
+    packages=['insta485'],
+    include_package_data=True,
+    install_requires=[
+        'arrow',
+        'bs4',
+        'Flask',
+        'html5validator',
+        'pycodestyle',
+        'pydocstyle',
+        'pylint',
+        'pytest',
+        'pytest-mock',
+        'requests',
+    ],
+    python_requires='>=3.6',
+)
