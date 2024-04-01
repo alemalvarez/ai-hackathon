619825da64028bd0d3427c0fd9e78a1e36191be6
Alejandro Mata <alemalvarez@icloud.com>
2024-03-31 17:15:00 -0700

    Use relative path in backend requests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
     try {
       console.log('Submitting form...');
       
-      const response = await fetch('http://localhost/response/', {
+      const response = await fetch('/response', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -77,7 +77,7 @@ function App() {
     try {
       console.log('Sending GET request...');
       
-      const response = await fetch('http://localhost/response/');
+      const response = await fetch('/response');
       console.log('Response received:', response);
 
       const data = await response.json();
