81fc7dd3b3c271c9377685552f0e773960314459
vaibhavjain2609 <vaibhavjain@protonmail.com>
2024-04-04 13:00:25 -0700

    added firebaseConfig

---
+++ frontend/src/firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCMQ5ySUTlpm_epg86T3U14bp7HaM2Sb_s",
+  authDomain: "crunchifyai.firebaseapp.com",
+  projectId: "crunchifyai",
+  storageBucket: "crunchifyai.appspot.com",
+  messagingSenderId: "221109441200",
+  appId: "1:221109441200:web:3f7bb51af2117856dee175",
+  measurementId: "G-X53FDSSLYF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
